4f09d403291018b8026fedd31a9e4babc9699404
Bailey Beck <baileymbeck@gmail.com>
2020-02-06 14:45:16 -0600

    notes route fix

--- plant_monitor/notes/views.py
+++ plant_monitor/notes/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-
 # Home f(x) to handle traffic from the homepage
 def home(request):
     return HttpResponse('<h1> Notes Home</h1>')
+
+def about(request):
+    return HttpResponse('<h1>Notes About</h1>')
