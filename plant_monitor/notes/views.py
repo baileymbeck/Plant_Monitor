fc64b4b64577bb961ab376c343b1e9bd7cfac447
Bailey Beck <baileymbeck@gmail.com>
2020-02-20 12:46:31 -0600

    dummy data, note content setup

--- plant_monitor/notes/views.py
+++ plant_monitor/notes/views.py
@@ -3,10 +3,33 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# dummy data
+
+posts = [
+    {
+        'author': 'Bailey',
+        'title': 'Notes post 1',
+        'content': 'first note content',
+        'date_posted': 'February 19, 2020'
+    },
+    {
+        'author': 'Sean',
+        'title': 'Notes post 2',
+        'content': 'second note content',
+        'date_posted': 'February 20, 2020'
+    }
+]
+
 # Home f(x) to handle traffic from the homepage
+
+
 def home(request):
+    context = {
+        'posts': posts
+    }
     # return HttpResponse('<h1> Notes Home</h1>')
-    return render(request, 'notes/home.html')
+    return render(request, 'notes/home.html', context)
+
 
 def about(request):
     # return HttpResponse('<h1>Notes About</h1>')
