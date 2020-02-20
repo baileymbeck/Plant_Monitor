dea6882380ebfe81272eb2c13b469b1226e73e36
Bailey Beck <baileymbeck@gmail.com>
2020-02-20 14:00:03 -0600

    dummy data for humidity sensor

--- plant_monitor/humidity_sensor/views.py
+++ plant_monitor/humidity_sensor/views.py
@@ -3,7 +3,27 @@ from django.shortcuts import render
 
 # Create your views here.
 
+#dummy data
+
+humidity_data = [
+    {
+        'Humidity': '10%',
+        'Temperature': '68 F',
+        'date': 'February 19, 2020',
+        'time': '21:21:21',  
+    },
+    {
+        'Humidity': '12%',
+        'Temperature': '70 F',
+        'date': 'February 19, 2020',
+        'time': '22:22:22',  
+    }
+]
+
 # Home f(x) to handle traffic from the homepage
 def home(request):
+    context = {
+        'humidity_data': humidity_data
+    }
     # return HttpResponse('<h1> Humidity Home</h1>')
-    return render(request, 'humidity_sensor/home.html')
+    return render(request, 'humidity_sensor/home.html', context)
